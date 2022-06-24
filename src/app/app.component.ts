58b3cd06ba2c3e5167f35d0a447fc14bd590da9d
viveksai26 <viveksai26@gmail.com>
2022-06-24 22:05:43 +0530

    Compare

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,12 +48,16 @@ export class AppComponent {
       "message": "Test1"
     }
   }`
+  formattedJsonForTable2: any;
+  formattedJson2: any;
+  myError2: any;
+  combinedDataTable: any;
   constructor(private formBuilder: FormBuilder) {}
   title = 'JSONTester';
   formattedJson:any;
   myError: any;
   jsonFormGroup: FormGroup = {} as FormGroup;
-  displayedColumns: any = ['name', 'value'];
+  displayedColumns: any = ['name', 'value', 'value2', 'name2'];
   ngOnInit(): void {
     this.formGenerator();
     this.jsonFormGroup.valueChanges.subscribe((data) => {
@@ -63,13 +67,40 @@ export class AppComponent {
         for (let x in fJSON) {
           this.formattedJsonForTable.push({name:x, value: fJSON[x]})
         }
-        console.log(this.formattedJsonForTable);
+        this.formattedJsonForTable.sort((a:any, b: any) => {return a.name.localeCompare(b.name)})
         this.formattedJson = fJSON;
         this.myError = null;
       } catch (error) {
         this.myError = error;
         this.formattedJson = null;
         this.formattedJsonForTable = [];
+      }
+      try {
+        const fJSON = this.recursionTest({}, '',JSON.parse(data.json2?.replace(/'/g, '"' )));
+        this.formattedJsonForTable2 = [];
+        for (let x in fJSON) {
+          this.formattedJsonForTable2.push({name:x, value: fJSON[x]})
+        }
+        this.formattedJsonForTable2.sort((a:any, b: any) => {return a.name.localeCompare(b.name)})
+        this.formattedJson2 = fJSON;
+        this.myError2 = null;
+      } catch (error) {
+        this.myError2 = error;
+        this.formattedJson2 = null;
+        this.formattedJsonForTable2 = [];
+      }
+      this.combinedDataTable = [];
+      let maxLength =  this.formattedJsonForTable2.length > this.formattedJsonForTable.length ? this.formattedJsonForTable2.length : this.formattedJsonForTable.length;
+      for (let i=0; i<maxLength; i+=1) {
+        this.combinedDataTable.push(
+          {
+          name: this.formattedJsonForTable[i]?.name,
+          value: this.formattedJsonForTable[i]?.value,
+          name2: this.formattedJsonForTable2[i]?.name,
+          value2: this.formattedJsonForTable2[i]?.value,
+          highlightRowValue: this.formattedJsonForTable[i]?.value !== this.formattedJsonForTable2[i]?.value,
+          highlightRowName: this.formattedJsonForTable[i]?.name !== this.formattedJsonForTable2[i]?.name
+        })
       }
     });
     this.jsonFormGroup.patchValue({});
@@ -112,6 +143,7 @@ export class AppComponent {
   formGenerator() {
     this.jsonFormGroup = this.formBuilder.group({
       json: [this.sampleJSON, Validators.required],
+      json2: [this.sampleJSON, Validators.required],
     });
   }
 }
